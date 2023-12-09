3f7275f98251d897edbee85e53468eea049b3a1b
MarkGiddings202 <gavingiddings1997@gmail.com>
2023-12-09 18:02:48 -0500

    basic objects again but this time with array-string notation

--- basic-objects/objects.js
+++ basic-objects/objects.js
@@ -4,19 +4,27 @@ let person = {
   age: 25,
   kids: 0,
   education: "Clara Barton h.s",
+  // added a key with strings incase I have a two word key name
+  "my goals": "Be a phenomal teacher!"
 };
 // access an object
 console.log(person.name);
 console.log(person.age);
 console.log(person.kids);
 console.log(person.education);
+// using the array string notation to log person object
+console.log(person["my goals"])
 
 // update the value of an object
 person.age = 26;
 person.education = "THE MARCY LAB SCHOOL";
 person["kids"] = 1;
+// using the array string notation to update person object
+person["my goals"] = "Buy a house!"
+
 // updated values
 console.log("--- Updated values ---");
 console.log(person.age);
 console.log(person.education);
 console.log(person.kids, "SOON LOL");
+console.log(person["my goals"])
